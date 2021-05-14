69f39ff99a613a36d5f5a2d2e48219b7a74110a7
ubittner <ulrich.bittner@me.com>
2021-05-14 09:52:42 +0200

    updates @ 5.1-10 (20210514)

--- HmIP-ASIR/module.php
+++ HmIP-ASIR/module.php
@@ -268,6 +268,32 @@ class QuittungstonHmIPASIR extends IPSModule
                 'MessageDescription'    => $messageDescription,
                 'rowColor'              => $rowColor];
         }
+        // Status
+        $formData['status'][0] = [
+            'code'    => 101,
+            'icon'    => 'active',
+            'caption' => 'Quittungston HmIP-ASIR wird erstellt',
+        ];
+        $formData['status'][1] = [
+            'code'    => 102,
+            'icon'    => 'active',
+            'caption' => 'Quittungston HmIP-ASIR ist aktiv (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][2] = [
+            'code'    => 103,
+            'icon'    => 'active',
+            'caption' => 'Quittungston HmIP-ASIR wird gelöscht (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][3] = [
+            'code'    => 104,
+            'icon'    => 'inactive',
+            'caption' => 'Quittungston HmIP-ASIR ist inaktiv (ID ' . $this->InstanceID . ')',
+        ];
+        $formData['status'][4] = [
+            'code'    => 200,
+            'icon'    => 'inactive',
+            'caption' => 'Es ist Fehler aufgetreten, weitere Informationen unter Meldungen, im Log oder Debug! (ID ' . $this->InstanceID . ')',
+        ];
         return json_encode($formData);
     }
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![IP-Symcon is awesome!](https://img.shields.io/badge/IP--Symcon-5.5-blue.svg)](https://www.symcon.de)
 [![Module Version](https://img.shields.io/badge/Module_Version-5.1-blue.svg)]()
-[![Module Build](https://img.shields.io/badge/Module_Build-9-blue.svg)]()
+[![Module Build](https://img.shields.io/badge/Module_Build-10-blue.svg)]()
 [![Module Date](https://img.shields.io/badge/Module_Date-20210514-blue.svg)]()  
 [![Check Style](https://github.com/ubittner/Quittungston/workflows/Check%20Style/badge.svg)](https://github.com/ubittner/Quittungston/actions)
 [![Run Tests](https://github.com/ubittner/Quittungston/workflows/Run%20Tests/badge.svg)](https://github.com/ubittner/Quittungston/actions)  
